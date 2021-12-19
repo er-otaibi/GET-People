39ea755a40c259d2b281770807a05191e5554d16
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-12-19 11:33:12 +0300

    Initial Commit

---
+++ Star Wars Encyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by Mac on 15/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
